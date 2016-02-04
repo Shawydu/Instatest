a93c39488256ed60ff7f604c1822794ef2ec5d7f
Shawydu <dxydyl2@gmail.com>
2016-02-03 19:32:19 -0500

    Initial test for basic controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'instas#index'
+  resources :instas
 end
--- spec/controllers/instas_controller_spec.rb
+++ spec/controllers/instas_controller_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe InstasController, type: :controller do
 		end
 	end
 
+	describe "instas#new action" do
+	    it "should successfully show the new form" do
+		    get :new
+		    expect(response).to have_http_status(:success)
+	    end
+	end
+
+	describe "instas#create action" do
+		it "should successfully submit the new form" do
+			post :create, gram: {message: 'Hello!'}
+			expect(response).to redirect_to root_path
+
+			gram = Gram.last
+			expect(gram.message).to eq("Hello!")
+		end
+	end
 end
